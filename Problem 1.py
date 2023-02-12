c227b9aabc93cd1fee99ecb9f192f12c844b2862
yis1234 <yis@middleton.school.nz>
2023-02-13 12:28:54 +1300

    Snake_1

--- Problem 1.py
+++ Problem 1.py
@@ -3,3 +3,8 @@ attempt = 0
 while attempt != pin:
     attempt = int(input("Enter pin: "))
 print("Correct")
+
+
+money = float(input("Please insert money: "))
+print(f"{money:.2f}")
+
---
+++ snake_1.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+
+
+pygame.quit()
+quit()
